d390e74dca669150e9020de7ad8a4f807d125fb0
JBSWAT <127140559+JBSWAT@users.noreply.github.com>
2023-03-08 20:21:06 +0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+#hallo world
+#ask the user for their name
+name = input("what's your name ?").strip() .  title()
+
+#remove whitespace from the str
+name = name.strip()
+#capitalize name of the user
+
+name = name.title()
+
+#print hello
+print(f"hallo,{name}")
